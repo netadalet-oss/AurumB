8539889fe03a3eb198b5a7419f381f35fceecfd1
netadalet-oss <netadalet@gmail.com>
2026-09-23 23:25:49 +0200

    Allow official Altinkaynak market services

--- app/src/main/java/com/aurum/bistterminal8/NativeMarketHttp.kt
+++ app/src/main/java/com/aurum/bistterminal8/NativeMarketHttp.kt
@@ -25,7 +25,8 @@ object NativeMarketHttp {
         "script.google.com", "script.googleusercontent.com",
         "theunat.com", "www.theunat.com",
         "borsaistanbulcanli.com", "www.borsaistanbulcanli.com",
-        "api.genelpara.com"
+        "api.genelpara.com",
+        "static.altinkaynak.com", "www.altinkaynak.com", "altinkaynak.com"
     )
 
     fun allowed(url: URL): Boolean =
