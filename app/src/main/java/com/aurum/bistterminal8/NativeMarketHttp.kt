d372d8b1ec6f7104efff7cf104e43bb65a997e72
netadalet-oss <netadalet@gmail.com>
2026-09-24 13:50:35 +0200

    Allow NOS RSS host in Android bridge

--- app/src/main/java/com/aurum/bistterminal8/NativeMarketHttp.kt
+++ app/src/main/java/com/aurum/bistterminal8/NativeMarketHttp.kt
@@ -22,7 +22,7 @@ object NativeMarketHttp {
         "stooq.com", "www.stooq.com",
         "www.kap.org.tr", "kap.org.tr",
         "www.borsaistanbul.com", "borsaistanbul.com",
-        "news.google.com", "www.tcmb.gov.tr", "tcmb.gov.tr",
+        "news.google.com", "feeds.nos.nl", "www.tcmb.gov.tr", "tcmb.gov.tr",
         "script.google.com", "script.googleusercontent.com",
         "theunat.com", "www.theunat.com",
         "borsaistanbulcanli.com", "www.borsaistanbulcanli.com",
